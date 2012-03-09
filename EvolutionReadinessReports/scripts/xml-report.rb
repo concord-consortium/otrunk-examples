9658f495ce6e5700538c28462330d306c0252be7
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2012-03-09 02:57:27 +0000

    Fix getting of first choice
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/otrunk/otrunk-examples@25766 6e01202a-0783-4428-890a-84243c50cc2b

--- EvolutionReadinessReports/scripts/xml-report.rb
+++ EvolutionReadinessReports/scripts/xml-report.rb
@@ -186,8 +186,9 @@ class XmlReport
   end
 
   def _doChoiceAnswerElem(answerElem, question)
-    # currentChoices = _getCurrentChoices(question.input)  # this gets current answer, but we always want first answer
-    currentChoices = question.incorrectAnswers.size > 0 ? question.incorrectAnswers.get(0) : question.input
+    ## currentChoices = _getCurrentChoices(question.input)  # this gets current answer, but we always want first answer
+    firstInput = question.incorrectAnswers.size > 0 ? question.incorrectAnswers.get(0) : question.input;
+    currentChoices = _getCurrentChoices(firstInput)
     if currentChoices.size == 0
       STDERR.puts("    no answer")
       answerElem.text = 'NO_ANSWER'
